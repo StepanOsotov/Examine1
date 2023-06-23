79639374bbe490d85779d9c43a54f30642ba1e56
Stepan <38191330+StepanOsotov@users.noreply.github.com>
2023-06-23 08:13:36 +0300

    Create main.h

---
+++ FastCgiC++/inc/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+//-----------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdint-gcc.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+//-----------------------------------------------------------------------------
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <curl/curl.h>
+//-----------------------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fcgio.h>
+//-----------------------------------------------------------------------------
+#include <vector>
+//-----------------------------------------------------------------------------
+using namespace std;
+//-----------------------------------------------------------------------------
+#endif	//MAIN_H
